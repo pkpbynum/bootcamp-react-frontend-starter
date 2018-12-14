e42bfc0257218d2da8312b9b54d83113e57f5a7f
Spencer Tiberi <spencertiberi@gmail.com>
2018-12-14 18:03:51 -0500

    Initialize starter repo

---
+++ src/containers/Home/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Welcome from '../../components/Welcome'
+
+class Home extends Component {
+  render() {
+    return <Welcome />
+  }
+}
+
+export default Home
