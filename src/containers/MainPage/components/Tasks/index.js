d1447a18e66de381291d4e41fbff7a6b2968d30c
Peter Bynum <pkpbynum@gmail.com>
2019-01-23 20:31:39 -0500

    worked on tasks page

--- src/containers/MainPage/components/Tasks/index.js
+++ src/containers/MainPage/components/Tasks/index.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { Container, Title } from "./styles";
+import TasksDropdown from "./subcomponents/TasksDropdown";
 
 class Tasks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterId: "0"
+    };
+  }
+
+  setFilterId = id => {
+    this.setState({ filterId: id });
+  };
+
   render() {
-    return <h1>Tasks Page!</h1>;
+    return (
+      <Container>
+        <Title>Tasks</Title>
+        <TasksDropdown
+          filtered={this.state.filterId}
+          setFilterId={this.setFilterId}
+        />
+      </Container>
+    );
   }
 }
 
---
+++ src/containers/MainPage/components/Tasks/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import { Flex } from "grid-styled";
+
+export const Container = styled(Flex)`
+  flex-direction: column;
+  flex: 1;
+  background: ${({ theme }) => theme.colors.rajah};
+  padding: 30px;
+`;
+
+export const Title = styled.p`
+  font-family: ${({ theme }) => theme.fonts.roboto.family};
+  font-weight: 700;
+  font-size: 3em;
+  margin-bottom: 30px;
+  user-select: none;
+`;
